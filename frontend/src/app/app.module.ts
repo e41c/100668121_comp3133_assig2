ad2c19028b85ca79121f7fd7d8fc0f79dc1ed26d
e41c <ericgrigor@gmail.com>
2024-04-08 11:52:46 -0400

    added apollo

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,29 @@
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+  constructor(
+    apollo: Apollo,
+    httpLink: HttpLink
+  ) {
+    apollo.create({
+      link: httpLink.create({ uri: 'http://localhost:4000/graphql' }), // Replace with your GraphQL endpoint
+      cache: new InMemoryCache()
+    });
+  }
+}
